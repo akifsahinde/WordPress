b8e515e2c17c994da00f2797875e1a668bdc2fa0
akifsahinde <37773836+akifsahinde@users.noreply.github.com>
2018-03-25 15:32:16 +0200

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Dieses Template zeigt eine einfache Möglichkeit für die Ausgabe eines Kategorie-Titels und einer Kategorie-Description in WordPress. 
- * Der Titel ist ein <div> Element eingebettet mit einer Ausrichtung und Klasse. Außerdem wird die <h1> dem Kategorie-Titel zugewiesen.
- * Der Eintrag unter WordPress in der Kategorie, dient als Kategorie Description. Dieser kann ebenfalls ausgegeben werden.
- * Es wurde im gleichen <div> abgelegt, könnte aber auch eine eigene Klasse und Ausrichtung zugewiesen bekommen.
+ * Diese Codezeilen für Archive.php zeigen eine einfache Möglichkeit für die Ausgabe eines Kategorie-Titels und einer 
+ * Kategorie-Description in WordPress. Der Titel ist ein <div> Element eingebettet mit einer Ausrichtung und Klasse. 
+ * Außerdem wird die <h1> dem Kategorie-Titel zugewiesen. Der Eintrag unter WordPress in der Kategorie, dient als 
+ * Kategorie Description. Dieser kann ebenfalls ausgegeben werden. Es wurde im gleichen <div> abgelegt, könnte aber 
+ * auch eine eigene Klasse und Ausrichtung zugewiesen bekommen.
  *
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
