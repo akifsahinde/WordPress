29b5f174401476a5b0302bbb21fb4d89bb8396c3
Akif Sahin <37773836+akifsahinde@users.noreply.github.com>
2018-05-13 11:11:56 +0200

    Update functions.php
    Funktion für den Aufruf bestimmter Fonts und Integration in CSS hinzugefügt.

--- functions.php
+++ functions.php
@@ -10,3 +10,51 @@ function tj_dequeue_devicepx() {
     wp_dequeue_script( 'devicepx' );
 }
 add_action( 'wp_enqueue_scripts', 'tj_dequeue_devicepx' );
+
+/**
+ *  Fonts in das eigene Theme integrieren, über eine Funktion für das im Einsatz befindliche Theme. In diesem Fall Tabor
+*/
+
+if ( ! function_exists( 'tabor_fonts_url' ) ) :
+	/**
+	 * Register custom fonts.
+	 */
+	function tabor_fonts_url() {
+		$fonts_url = '';
+
+		/*
+		 * Translators: If there are characters in your language that are not
+		 * supported by Heebo, translate this to 'off'. Do not translate
+		 * into your own language.
+		 */
+		$heebo = esc_html_x( 'on', 'Heebo font: on or off', 'tabor' );
+
+		/*
+		 * Translators: If there are characters in your language that are not
+		 * supported by Lora, translate this to 'off'. Do not translate
+		 * into your own language.
+		 */
+		$lora = esc_html_x( 'on', 'Lora font: on or off', 'tabor' );
+
+		if ( 'off' !== $heebo || 'off' !== $lora ) {
+			$font_families = array();
+
+			if ( 'off' !== $heebo ) {
+				$font_families[] = 'Heebo:400,500,800';
+			}
+
+			if ( 'off' !== $lora ) {
+				$font_families[] = 'Lora:400,400i,700';
+			}
+
+			$query_args = array(
+				'family' => rawurlencode( implode( '|', $font_families ) ),
+				'subset' => rawurlencode( 'latin,latin-ext' ),
+			);
+
+			$fonts_url = add_query_arg( $query_args, 'https://www.akifsahin.de/css' );
+		}
+
+		return esc_url_raw( $fonts_url );
+	}
+endif;
