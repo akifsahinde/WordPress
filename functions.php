e2f7f2e114f47740e90325fbd7e528c835f06338
Akif Sahin <37773836+akifsahinde@users.noreply.github.com>
2018-05-13 13:01:42 +0200

    Update functions.php
    Functions.php um weitere Angaben ergänzt. Es beginnt mit einer Einbindung des Original Stylesheets für das Child-Theme und die Rangfolge. Neu ist auch eine Funktion, die Google Webfonts als CSS herausfiltert und auch die Speicherung von IP-Adressen bei Kommentaren ausschließt. (wichtig für DSGVO)

--- functions.php
+++ functions.php
@@ -1,3 +1,32 @@
+<?php
+/**
+* Child theme stylesheet einbinden in Abhängigkeit vom Original-Stylesheet
+*/
+
+function child_theme_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+
+
+/**
+* Google Fonts aus dem Haupttheme entfernen
+*/
+
+function removeGoogleFonts(){
+		global $wp_styles;
+			$regex = '/fonts\.googleapis\.com\/css\?family/i';
+			foreach($wp_styles->registered as $registered) {
+
+				if( preg_match($regex, $registered->src) ) {
+					wp_dequeue_style($registered->handle);
+				}
+			}
+		}
+add_action('wp_enqueue_scripts', 'removeGoogleFonts', 999);
+
 /**
  *  Einträge und Ressourcen-Hinweise, wie wp.org verbergen
  */
@@ -12,9 +41,12 @@ function tj_dequeue_devicepx() {
 add_action( 'wp_enqueue_scripts', 'tj_dequeue_devicepx' );
 
 /**
- *  Funktion um IP-Adressen bei Kommentaren rauszufiltern
- */
+* IP-Adressen bei Kommentaren entfernen
+*/
+
 function wp_remove_commentip( $comment_author_ip ) {
 return '';
 }
 add_filter( 'pre_comment_user_ip', 'wp_remove_commentip' );
+
+?>
